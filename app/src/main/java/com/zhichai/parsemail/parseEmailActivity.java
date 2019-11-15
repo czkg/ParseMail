f6ba68ab8bdfd8c7199bc75724878491610fbb0f
czkg <cz@zhis-imac-pro.home>
2019-11-15 00:21:45 -0500

    11/15

--- app/src/main/java/com/zhichai/parsemail/parseEmailActivity.java
+++ app/src/main/java/com/zhichai/parsemail/parseEmailActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.zhichai.parsemail.EmailReceiver;
 import com.zhichai.parsemail.Email;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class parseEmailActivity extends AppCompatActivity {
+    final EditText startTimeText = (EditText) findViewById(R.id.startTimeText);
+    final EditText endTimeText = (EditText) findViewById(R.id.endTimeText);
+    final Button startTimeButton = (Button) findViewById(R.id.startTimeButton);
+    final Button endTimeButton = (Button) findViewById(R.id.endTimeButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +45,19 @@ public class parseEmailActivity extends AppCompatActivity {
 
     }
 
-    public void setStartTime(View view) {
-        
+    public void onCreateDateDialog(View v) {
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                txtDate.setText(day + "-" + (month + 1) + "-" + year);
+            }
+        }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 
-    public void setEndTime(View view) {
-
-    }
 }
